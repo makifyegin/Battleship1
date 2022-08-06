9270e149f8478d14d91c706b89766cc4267ebd12
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-06 05:27:36 +0100

    create a 10x10 field to use Multidimensional Arrays

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here
+        char[][] field = new char[10][10];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = '~';
+            }
+        }
     }
 }

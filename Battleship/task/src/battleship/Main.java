1d5bef9899e4823f33dcfd83ac40db286816d1d1
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-06 11:12:48 +0100

    I create a table for play BattleShip ...

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -8,16 +8,17 @@ Field fieldA = new Field();
 
     public static void main(String[] args) {
         // Write your code here
-        String[][] field = new String[10][10];
-        System.out.println();
+        String[][] field = new String[10][10];  //Multidimensional Array[10][10] as a String
         for (int i = 0; i < field.length; i++) {
                 for (int j = 0; j < field[i].length; j++) {
                     field[i][j] = "~";
-
                 }
             }
+        System.out.print("  ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
         System.out.println();
-
         for (int i = 0, l= 'A'; i < field.length; i++,l++) {
             System.out.print("" + Character.toString(l) + " ");
             for (int j = 0; j < field[i].length; j++) {

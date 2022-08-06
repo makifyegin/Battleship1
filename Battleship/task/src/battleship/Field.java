77f5f32da9d274d77ce81f1e5d7428c7e1b1e561
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-06 13:25:50 +0100

    I implement an oop concept of inheritance

--- Battleship/task/src/battleship/Field.java
+++ Battleship/task/src/battleship/Field.java
@@ -1,24 +1,48 @@
 package battleship;
 
+import java.util.Arrays;
+
 public class Field {
-    private int row =0;
-    private String column = "";
+
+    public static final String[][] myOcean = new String[10][10];  //Multidimensional Array[10][10] as a String
+
+    public static final String[] aircraft = new String[5];
+    public static final String[] battleship = new String[4];
+    public static final String[] submarine = new String[3];
+    public static final String[] cruise = new String[3];
+    public static final String[] destroyer = new String[2];
 
 
+    public  String[][] createOcean() {
+        for (int i = 0; i < myOcean.length; i++) {
+            for (int j = 0; j < myOcean[i].length; j++) {
+                myOcean[i][j] = "~";
+            }
 
-    public int getRow() {
-        return row;
-    }
+        }
+        Arrays.fill(aircraft, 0, aircraft.length, "O");
+        Arrays.fill(battleship, 0, battleship.length, "O");
+        Arrays.fill(cruise, 0, cruise.length, "O");
+        Arrays.fill(destroyer, 0, destroyer.length, "O");
+        Arrays.fill(submarine, 0, submarine.length, "O");
+        System.out.print("  ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0, l = 'A'; i < myOcean.length; i++, l++) {
+            System.out.print("" + Character.toString(l) + " ");
+            for (int j = 0; j < myOcean[i].length; j++) {
+                System.out.print((myOcean[i][j]) + " ");
+            }
+            System.out.println();
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+            // ~ denotes the fog of war
+            // O denotes a cells with your ship
+            // X denotes that the ship was hit?
 
-    public String getColumn() {
-        return column;
-    }
 
-    public void setColumn(String column) {
-        this.column = column;
+        }
+        return myOcean;
     }
 }
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -2,34 +2,17 @@ package battleship;
 
 import java.util.Arrays;
 
-public class Main {
 
-Field fieldA = new Field();
+public class Main extends Field {
+
 
     public static void main(String[] args) {
+        Field fieldA = new Field();
         // Write your code here
-        String[][] field = new String[10][10];  //Multidimensional Array[10][10] as a String
-        for (int i = 0; i < field.length; i++) {
-                for (int j = 0; j < field[i].length; j++) {
-                    field[i][j] = "~";
-                }
-            }
-        //
-        System.out.print("  ");
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 0, l= 'A'; i < field.length; i++,l++) {
-            System.out.print("" + Character.toString(l) + " ");
-            for (int j = 0; j < field[i].length; j++) {
-                System.out.print((field[i][j]) + " ");
-            }
-            System.out.println();
 
-            // ~ denotes the fog of war
-            // O denotes a cells with your ship
-            // X denotes that the ship was hit?
-        }
+
     }
+
+
 }
+

36563d2c97fd4cf490ec07caaf30bac1b030b353
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-06 05:39:13 +0100

    I added a print that was printed by usinf nested loops

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -1,14 +1,23 @@
 package battleship;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         // Write your code here
-        char[][] field = new char[10][10];
+        String[][] field = new String[10][10];
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                field[i][j] = '~';
+                field[i][j] = "~";
             }
+        }
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print((field[i][j]) + " ");
+            }
+            System.out.println();
+
         }
     }
 }

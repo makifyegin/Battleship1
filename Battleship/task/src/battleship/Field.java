02be4002971f8d035023c0f8a3a6d2a28a977182
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-06 10:49:48 +0100

    I printed first row of columns with A to J

---
+++ Battleship/task/src/battleship/Field.java
@@ -0,0 +1,24 @@
+package battleship;
+
+public class Field {
+    private int row =0;
+    private String column = "";
+
+
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+}
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -4,15 +4,22 @@ import java.util.Arrays;
 
 public class Main {
 
+Field fieldA = new Field();
+
     public static void main(String[] args) {
         // Write your code here
         String[][] field = new String[10][10];
+        System.out.println();
         for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[i].length; j++) {
-                field[i][j] = "~";
+                for (int j = 0; j < field[i].length; j++) {
+                    field[i][j] = "~";
+
+                }
             }
-        }
-        for (int i = 0; i < field.length; i++) {
+        System.out.println();
+
+        for (int i = 0, l= 'A'; i < field.length; i++,l++) {
+            System.out.print("" + Character.toString(l) + " ");
             for (int j = 0; j < field[i].length; j++) {
                 System.out.print((field[i][j]) + " ");
             }

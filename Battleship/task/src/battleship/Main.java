50c84f658dcdc62753763483aad2ef48b5eb9a8f
m.akifyegin <mehmetakifyegin@icloud.com>
2022-08-09 12:16:46 +0100

    I implement some logic that if that's not equal the ships length give me the error or if you don't put the vertical or horizantal

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -6,7 +6,15 @@ import java.util.Scanner;
 
 
 public class Main extends Field {
-
+    public static String firstColumnCharacter1;
+    public static String secondColumnCharacter1;
+    public static String firstColumnCharacter2;
+    public static String secondColumnCharacter2;
+    public static int secondColumnInteger1;
+    public static int secondColumnInteger2;
+    public static String firstColumn;
+    public static String secondColumn;
+    public static int dif;
 
     public static void main(String[] args) {
         Field firstPlayer = new Field();
@@ -15,26 +23,59 @@ public class Main extends Field {
         String[][] secondPlayerOcean = new String[10][10];
         // Write your code here
         Scanner scanner = new Scanner(System.in);
-        String firstColumn = scanner.next();
-        String secondColumn = scanner.next();
-        String firstColumnCharacter = "" + firstColumn.charAt(0);
-        String secondColumnCharacter = "" + firstColumn.charAt(1);
+        firstColumn = scanner.next();
+        secondColumn = scanner.next();
 
-
-
-        String x = "" + secondColumn.charAt(0);
-        String secondColumnCharacterSecond = "" + secondColumn.charAt(0);
-
-
-
-        int secondColumnInteger = Integer.parseInt(secondColumnCharacter) - 1;
         firstPlayerOcean = firstPlayer.createOcean();
-        firstPlayerOcean[column(firstColumnCharacter)][secondColumnInteger] = "O";
-        firstPlayerOcean[column(secondColumnCharacterSecond)][secondColumnInteger] = "O";
+
+        firstColumnCharacter1 = "" + firstColumn.charAt(0);
+        secondColumnCharacter1 = "" + firstColumn.charAt(1);
+        firstColumnCharacter2 = "" + secondColumn.charAt(0);
+        secondColumnCharacter2 = "" + secondColumn.charAt(1);
+        secondColumnInteger1 = Integer.parseInt(secondColumnCharacter1) - 1;
+        secondColumnInteger2 = Integer.parseInt(secondColumnCharacter2) - 1;
+
+        dif = secondColumnInteger2 - secondColumnInteger1;
+        firstPlayerOcean[column(firstColumnCharacter1)][secondColumnInteger1] = "O";
+        firstPlayerOcean[column(firstColumnCharacter2)][secondColumnInteger2] = "O";
 
         printMultiDimensionalArray(firstPlayerOcean);
 
 
     }
-}
+
+    public static int count = 0;
+
+    public static int findCharactersSubtrack(String first, String second) {
+
+        for (char i = first.charAt(0); i < second.charAt(0); i++) {
+
+            count++;
+        }
+
+        return count;
+    }
+
+
+    public static String[][] putShip(String[] akif) {
+
+        if (firstColumnCharacter1.equals(firstColumnCharacter2) || (secondColumnInteger1 == secondColumnInteger2)) {
+            if (Math.abs(dif) == akif.length || findCharactersSubtrack(firstColumnCharacter1,firstColumnCharacter2)==akif.length) {
+
+                if (Math.abs(dif)==akif.length) {
+                    for (int i = secondColumnInteger1; i <=secondColumnInteger2 ; i++) {
+
+
+
+                    }
+                }
+
+
+
+
+            }
+            }
+
+        }
+    }
 
